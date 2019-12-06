ce5c8a484e11aaf073ad09fbc53a1c727ec44dff
verjeff <58576125+verjeff@users.noreply.github.com>
2019-12-06 08:40:01 +0800

    Add files via upload

---
+++ Mains.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mains;
+
+/**
+ *
+ * @author Comlab
+ */
+public class Mains {
+
+
+  public static void main(String[] args) {
+      String name = "Jeff Besoña";
+    System.out.println("Hello World  " + name);
+  }
+
+}
